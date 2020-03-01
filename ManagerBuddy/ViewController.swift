750f09884dff24a7926b6c08000c45539e5d50c8
Amit Gajjar <amitgajjar510@gmail.com>
2020-03-01 23:27:21 +0530

    Initial Commit

---
+++ ManagerBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManagerBuddy
+//
+//  Created by Amit Gajjar on 01/03/20.
+//  Copyright © 2020 amitgajjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
