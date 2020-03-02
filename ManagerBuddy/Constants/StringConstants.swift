bc4bbc96172b4577f99dcabe9e6081c32ecb6664
Amit Gajjar <amitgajjar510@gmail.com>
2020-03-02 20:55:37 +0530

    Basic constants files created

---
+++ ManagerBuddy/Constants/StringConstants.swift
@@ -0,0 +1,9 @@
+//
+//  StringConstants.swift
+//  ManagerBuddy
+//
+//  Created by Amit Gajjar on 02/03/20.
+//  Copyright © 2020 amitgajjar. All rights reserved.
+//
+
+import Foundation
---
+++ ManagerBuddy/Constants/URLConstants.swift
@@ -0,0 +1,9 @@
+//
+//  URLConstants.swift
+//  ManagerBuddy
+//
+//  Created by Amit Gajjar on 02/03/20.
+//  Copyright © 2020 amitgajjar. All rights reserved.
+//
+
+import Foundation
